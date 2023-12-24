cbb66aeeb1e43b0ec95f6352e52960be47a76cfd
Dominik <dominikkawka2002@gmail.com>
2023-12-24 23:00:27 +0000

    added login/signup pages + slight model changes

--- frontend/src/components/FooterBar/FooterBar.jsx
+++ frontend/src/components/FooterBar/FooterBar.jsx
@@ -22,7 +22,7 @@ const goToTwitter = () => {
 function FooterBar() {
   return (
     <>
-    <footer style={{position: "fixed", bottom:0, width: '100%', background: "#004385", color: "#DBE4EE"}}>
+    <footer style={{position: "fixed", bottom:0, left:0, width: '100%', background: "#004385", color: "#DBE4EE"}}>
         <Container maxHeight="xl" sx={{ m: 2 }}>
             <Grid container spacing={0}>
               <Grid item>
