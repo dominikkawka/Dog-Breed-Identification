f6b5329168e8ee2947d080fb8efa098aca297d22
Dominik <dominikkawka2002@gmail.com>
2024-02-24 16:06:36 +0000

    Home Page Header

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import FileForm from "../components/FileForm/FileForm";
 import WebcamFeed from "../components/WebcamFeed/WebcamFeed";
+import HomeHeader from "../components/HomeHeader/homeHeader";
+import { Divider } from "@mui/material";
 
 const HomePage = () => {
   return (
    <>
       <div>
+      <HomeHeader />
+      <Divider />
+      <Divider />
       <p>Upload an image file!</p>
       <FileForm/>
       <br />
